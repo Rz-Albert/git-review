b1d15edec2f7943fa82d61fa565ef7c0ab34ed1a
Tzzent <rzcarlosalbert@gmail.com>
2024-10-25 17:45:14 -0500

    add: new variable 'number' no show in greeting fn

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const variable = "Hello"
 const second_variable = "World"
+const number = 12
 
 const greeting = () => {
-  console.log(`${variable} ${second_variable}`)
+  console.log(`${variable} ${second_variable}: ` + number)
 }
 
 greeting()
